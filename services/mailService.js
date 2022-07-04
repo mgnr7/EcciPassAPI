5566425a79166bad4f5855ed508d9239227af87b
Manuel Alejandro Fernandez Carrillo <b72892@ecci.ucr.ac.cr>
2022-07-04 14:33:27 -0600

    Estructura mailService

--- services/mailService.js
+++ services/mailService.js
@@ -1,5 +1,27 @@
 const nodemailer = require("nodemailer");
 
-const getTransporter = function () {};
+const getTransporter = function () {
+    let transporter;
+  transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  return transporter;
+};
 
-exports.senRecoveryCodeEmail = () => {};
+exports.senRecoveryCodeEmail = () => {
+    let transporter = getTransporter();
+    await transporter.sendMail({
+      /*Definir correo electronico*/
+      from: "",
+      to: userEmail,
+      subject: "Código de recuperación EcciPass",
+      text: `Utilice este código para recuperar su contraseña: ${randomToken}`,
+      html: `Utilice este código para recuperar su contraseña: <strong>${randomToken}</strong>`,
+    });
+};
